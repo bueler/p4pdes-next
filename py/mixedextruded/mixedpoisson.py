ebb6f97f734c888503a9cbb6d56ba8dcc227ce1f
Ed Bueler <elbueler@alaska.edu>
2020-09-08 19:03:33 -0800

    document solvers

--- py/mixedextruded/mixedpoisson.py
+++ py/mixedextruded/mixedpoisson.py
@@ -2,6 +2,15 @@
 
 # from section 3.1 of Gibson et al 2019
 
+# regarding solvers, the defaults (based on MATNEST) correspond to:
+#   ./mixedpoisson.py -s_ksp_converged_reason -s_ksp_type gmres -s_pc_type jacobi
+# alternatively here is a fast, and then a slow, direct solver, based on MATAIJ:
+#   ./mixedpoisson.py -s_ksp_converged_reason -s_mat_type aij \
+#       -s_pc_type lu -s_pc_factor_mat_ordering_type natural
+#   ./mixedpoisson.py -s_ksp_converged_reason -s_mat_type aij \
+#       -s_pc_type svd
+
+
 from firedrake import *
 
 m = 16
@@ -23,7 +32,7 @@ a = (dot(tau, sigma) - u * div(tau) + div(sigma) * v) * dx
 L = f*v*dx
 
 w = Function(W, name="Solution")
-solve(a == L, w)
+solve(a == L, w, options_prefix='s')
 
 sigmasoln, usoln = w.split()
 sigmasoln.rename('sigma')
